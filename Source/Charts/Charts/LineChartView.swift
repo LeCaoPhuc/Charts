8d1e0d992904eec285b32b2253a64266caef2a3d
Hoang JK <lexuanhoang2506@gmail.com>
2017-03-31 01:32:27 +0700

    Update LineChartView.swift

--- Source/Charts/Charts/LineChartView.swift
+++ Source/Charts/Charts/LineChartView.swift
@@ -23,6 +23,47 @@ public class BarChartFormatter: NSObject, IAxisValueFormatter{
     }
 }
 
+@objc(YAxisValueFormatter)
+public class YAxisValueFormatter: NSObject, IAxisValueFormatter {
+    
+    let numFormatter: NumberFormatter
+    
+    override init() {
+        numFormatter = NumberFormatter()
+        numFormatter.minimumFractionDigits = 0
+        numFormatter.maximumFractionDigits = 0
+        
+        // if number is less than 1 add 0 before decimal
+        numFormatter.minimumIntegerDigits = 1 // how many digits do want before decimal
+        numFormatter.paddingPosition = .beforePrefix
+        numFormatter.paddingCharacter = "0"
+    }
+    
+    public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+        return numFormatter.string(from: NSNumber(floatLiteral: value))!
+    }
+}
+
+@objc(ValueFormatter)
+public class ValueFormatter: NSObject, IValueFormatter {
+    let numFormatter: NumberFormatter
+    
+    override init() {
+        numFormatter = NumberFormatter()
+        numFormatter.minimumFractionDigits = 0
+        numFormatter.maximumFractionDigits = 0
+        
+        // if number is less than 1 add 0 before decimal
+        numFormatter.minimumIntegerDigits = 1 // how many digits do want before decimal
+        numFormatter.paddingPosition = .beforePrefix
+        numFormatter.paddingCharacter = "0"
+    }
+    
+    public func stringForValue(_ value: Double, entry: ChartDataEntry, dataSetIndex: Int, viewPortHandler: ViewPortHandler?) -> String {
+        return numFormatter.string(from: NSNumber(floatLiteral: value))!
+    }
+}
+
 /// Chart that draws lines, surfaces, circles, ...
 open class LineChartView: BarLineChartViewBase, LineChartDataProvider
 {
